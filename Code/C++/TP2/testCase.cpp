e551f5d1b1c4ae30063ead8c5c9d88d064e22fcf
Filipe Vitorino <filipe0vit@gmail.com>
2021-03-11 21:09:22 -0300

    Teste#2 - fibonacci

--- Code/C++/TP2/testCase.cpp
+++ Code/C++/TP2/testCase.cpp
@@ -6,9 +6,22 @@
 
 #include "../factorial.cpp"
 
-TEST_CASE("testing the factorial function") {
+TEST_CASE("Testando funcao fatorial") {
+    CHECK(fact(0) == 1);
     CHECK(fact(1) == 1);
     CHECK(fact(2) == 2);
     CHECK(fact(3) == 6);
     CHECK(fact(10) == 3628800);
 }
+
+
+
+#include "../fibonacci.cpp"
+TEST_CASE("Testando funcao fibonacci") {
+    CHECK(fib(0) == 0);
+    CHECK(fib(1) == 1);
+    CHECK(fib(2) == 1);
+    CHECK(fib(3) == 2);
+    CHECK(fib(20) == 6765);
+}
+
